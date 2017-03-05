6f734f2464a5f2cd8e80aab778789b3f3fa212c1
X_zzzzz <195286381@qq.com>
2017-03-06 00:11:38 +0800

    Update no_2.js

--- no_2.js
+++ no_2.js
@@ -5,19 +5,6 @@
 // Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 // Output: 7 -> 0 -> 8
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-
     var addTwoNumbers = function(l1, l2) {
         
         var isThanNine = false;
